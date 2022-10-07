2dbff8eff814c190826eae82e5921dc616e73c25
safiya <safiya-91@hotmail.co.uk>
2022-10-07 21:42:48 +0100

    add helpers.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-from flask import Flask
+from helpers import get_pokemon_by_name, get_random_pokemon_list
 
 
 app = Flask(__name__)
@@ -10,60 +7,17 @@ app = Flask(__name__)
 
 @app.get("/")
 def pokemon_list():
-    return "Bulbasaur, charmander, pikachu, eevee, diglett"
-
-
-@app.get("/bulbasaur")
-def bulbasaur_data():
-    return "This is bulbasaur, a generation 1 pokemon who looks like a tiny dinosaur"
-
-
-@app.get("/charmander")
-def charmander_data():
-    return "This is charmander, a generation 1 pokemon who looks like a tiny reptile"
-
-
-@app.get("/pikachu")
-def pikachu_data():
-    return "This is pikachu, a generation 1 pokemon who looks like a tiny rodent"
-
-
-@app.get("/eevee")
-def eevee_data():
-    return "This is eevee, a generation 1 pokemon who looks like a tiny fox"
-
-
-@app.get("/diglett")
-def diglett_data():
-    return "This is diglett, a generation 1 pokemon who looks like a tiny mole"
-
-
-if __name__ == "__main__":
-    app.run()
-from flask import Flask
-
-
-app = Flask(__name__)
-
-
-pokemon_creatures = {
-    "bulbasaur": "dinosaur",
-    "charmander": "reptile",
-    "pikachu": "rodent",
-    "eevee": "fox",
-    "diglett": "mole",
-}
-
-
-@app.get("/")
-def pokemon_list():
-    return "Bulbasaur, charmander, pikachu, eevee, diglett"
+    return ', '.join([pokemon['name'] for pokemon in get_random_pokemon_list()]).title()
 
 
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
-    creature = pokemon_creatures.get(pokemon_name)
-    return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}"
+    pokemon = get_pokemon_by_name(pokemon_name)
+    return f"This is {pokemon['name'].capitalize()}.\n" \
+           f"Height: {pokemon['height']}.\n" \
+           f"Weight: {pokemon['weight']}.\n" \
+           f"Base experience: {pokemon['base_experience']}.\n" \
+           f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
 
 
 if __name__ == "__main__":
