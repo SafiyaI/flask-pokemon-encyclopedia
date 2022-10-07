17cb6d98543ed44e4fb689525bdd1c47b4bb4366
safiya <safiya-91@hotmail.co.uk>
2022-10-07 20:44:43 +0100

    turn pokemon ino one function

--- app.py
+++ app.py
@@ -40,3 +40,31 @@ def diglett_data():
 
 if __name__ == "__main__":
     app.run()
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+pokemon_creatures = {
+    "bulbasaur": "dinosaur",
+    "charmander": "reptile",
+    "pikachu": "rodent",
+    "eevee": "fox",
+    "diglett": "mole",
+}
+
+
+@app.get("/")
+def pokemon_list():
+    return "Bulbasaur, charmander, pikachu, eevee, diglett"
+
+
+@app.get("/<pokemon_name>")
+def pokemon_data(pokemon_name):
+    creature = pokemon_creatures.get(pokemon_name)
+    return f"This is {pokemon_name}, a generation 1 pokemon who looks like a tiny {creature}"
+
+
+if __name__ == "__main__":
+    app.run()
